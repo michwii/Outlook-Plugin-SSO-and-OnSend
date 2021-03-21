a7acbf07ff976555e3eff6593e3bd47d139f0e1b
Michwii <freezerhm@yahoo.fr>
2021-03-21 15:53:53 +0100

    erasing the code that is not functionning

--- server.js
+++ server.js
@@ -24,24 +24,6 @@ app.get('/', (req, res) => {
    res.send('Now using https..');
 });
 
-app.post('/api/authenticate', async (req, res) => {
-  extractToken = function(header){
-    if (header){
-        const split = header.split(' ');
-        return split[1];
-    }else{
-        return "";
-    }
-  }
-  const bootstrapToken = extractToken(req.headers['authorization']);
-  const tenantId = '323d9c5b-c193-4c4f-8fb6-a029b2a10ca3';
-
-  var ADD_Helper = new AAD_HELPER(tenantId);
-  const accessToken = await ADD_Helper.exchangeBootstrapToken(bootstrapToken);
-
-  res.json(accessToken);
-});
-
 var server = https.createServer(options, app);
 
 server.listen(port, () => {
